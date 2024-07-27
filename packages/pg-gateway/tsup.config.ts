cfa72720f5ea766e35389d006c584939a85f05f0
Greg Richardson <greg.nmr@gmail.com>
2024-07-27 16:37:30 -0500

    chore: dual esm + cjs builds using tsup

---
+++ packages/pg-gateway/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    outDir: 'dist',
+    sourcemap: true,
+    dts: true,
+    minify: true,
+  },
+]);
