322451e8ef4febd0268aa52248035597c53f2755
Julien Goux <hi@jgoux.dev>
2024-08-08 08:44:00 +0200

    export BackendError

--- packages/pg-gateway/src/index.ts
+++ packages/pg-gateway/src/index.ts
@@ -2,3 +2,4 @@ export { default as PostgresConnection } from './connection.js';
 export * from './connection.js';
 export * from './auth/sasl/scram-sha-256.js';
 export * from './auth/md5.js';
+export * from './backend-error.js';
