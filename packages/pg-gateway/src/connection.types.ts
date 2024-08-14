988b9bbffbaeda1e4ca7568c155f89b45cba0b47
Julien Goux <hi@jgoux.dev>
2024-08-14 23:29:52 +0200

    chore: add callback form for serverVersion

--- packages/pg-gateway/src/connection.types.ts
+++ packages/pg-gateway/src/connection.types.ts
@@ -13,9 +13,18 @@ export type TlsInfo = {
   sniServerName?: string;
 };
 
+export const ServerStep = {
+  AwaitingInitialMessage: 'AwaitingInitialMessage',
+  PerformingAuthentication: 'PerformingAuthentication',
+  ReadyForQuery: 'ReadyForQuery',
+} as const;
+
+export type ServerStep = (typeof ServerStep)[keyof typeof ServerStep];
+
 export type ConnectionState = {
   hasStarted: boolean;
   isAuthenticated: boolean;
   clientInfo?: ClientInfo;
   tlsInfo?: TlsInfo;
+  step: ServerStep;
 };
