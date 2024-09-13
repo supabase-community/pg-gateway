4f7fc2421e30e1d495e4e88b9fc714520c547a23
Julien Goux <hi@jgoux.dev>
2024-09-13 12:10:35 +0200

    tests

--- packages/pg-gateway/test/node/tls.test.ts
+++ packages/pg-gateway/test/node/tls.test.ts
@@ -1,63 +1,15 @@
-import { createServer, type Server } from 'node:net';
-import type { ClientConfig } from 'pg';
-import { type PostgresConnectionOptions, createDuplexPair } from 'pg-gateway';
-import { fromDuplexStream, fromNodeSocket } from 'pg-gateway/node';
+import { createDuplexPair } from 'pg-gateway';
+import { fromDuplexStream } from 'pg-gateway/node';
 import { describe, expect, it } from 'vitest';
-import { DisposablePgClient, socketFromDuplexStream } from '../util';
-import { generateCA, generateCSR, signCert, toPEM } from './certs';
-import { once } from 'node:events';
-
-async function generateAllCertificates() {
-  const { caKey, caCert } = await generateCA('My Root CA');
-
-  const { key: serverKey, csr: serverCsr } = await generateCSR('localhost');
-  const serverCert = await signCert(caCert, caKey, serverCsr);
-
-  const { key: clientKey, csr: clientCsr } = await generateCSR('postgres');
-  const clientCert = await signCert(caCert, caKey, clientCsr);
-
-  const encoder = new TextEncoder();
-
-  return {
-    caKey: encoder.encode(toPEM(caKey, 'PRIVATE KEY')),
-    caCert: encoder.encode(toPEM(caCert, 'CERTIFICATE')),
-    serverKey: encoder.encode(toPEM(serverKey, 'PRIVATE KEY')),
-    serverCert: encoder.encode(toPEM(serverCert, 'CERTIFICATE')),
-    clientKey: encoder.encode(toPEM(clientKey, 'PRIVATE KEY')),
-    clientCert: encoder.encode(toPEM(clientCert, 'CERTIFICATE')),
-  };
-}
+import {
+  createPostgresClient,
+  createPostgresServer,
+  getPort,
+  socketFromDuplexStream,
+} from '../util';
+import { generateAllCertificates } from './certs';
 
 const { caCert, serverKey, serverCert, clientKey, clientCert } = await generateAllCertificates();
-
-async function createPostgresServer(options?: PostgresConnectionOptions) {
-  const server = createServer((socket) => fromNodeSocket(socket, options));
-
-  // Listen on a random free port
-  server.listen(0);
-  await once(server, 'listening');
-  return server;
-}
-
-function getPort(server: Server) {
-  const address = server.address();
-
-  if (typeof address !== 'object') {
-    throw new Error(`Invalid server address '${address}'`);
-  }
-
-  if (!address) {
-    throw new Error('Server has no address');
-  }
-
-  return address.port;
-}
-
-async function connectPg(config: string | ClientConfig) {
-  const client = new DisposablePgClient(config);
-  await client.connect();
-  return client;
-}
 
 describe('tls', () => {
   it('basic tls over tcp', async () => {
@@ -71,7 +23,7 @@ describe('tls', () => {
       },
     });
 
-    await using client = await connectPg({
+    await using client = await createPostgresClient({
       port: getPort(server),
       ssl: {
         ca: Buffer.from(caCert),
@@ -93,7 +45,7 @@ describe('tls', () => {
       },
     });
 
-    await using client = await connectPg({
+    await using client = await createPostgresClient({
       host: 'localhost',
       port: getPort(server),
       ssl: {
@@ -116,7 +68,7 @@ describe('tls', () => {
       },
     });
 
-    await using client = await connectPg({
+    await using client = await createPostgresClient({
       host: '127.0.0.1',
       port: getPort(server),
       ssl: {
@@ -137,7 +89,7 @@ describe('tls', () => {
       },
     });
 
-    await using client = await connectPg({
+    await using client = await createPostgresClient({
       port: getPort(server),
       user: 'postgres',
       ssl: {
@@ -160,7 +112,7 @@ describe('tls', () => {
       },
     });
 
-    const promise = connectPg({
+    const promise = createPostgresClient({
       port: getPort(server),
       user: 'bob',
       ssl: {
@@ -185,7 +137,7 @@ describe('tls', () => {
       },
     });
 
-    const promise = connectPg({
+    const promise = createPostgresClient({
       port: getPort(server),
       ssl: {
         ca: Buffer.from(serverCert),
@@ -209,7 +161,7 @@ describe('tls', () => {
       },
     });
 
-    await using client = await connectPg({
+    await using client = await createPostgresClient({
       stream: socketFromDuplexStream(clientDuplex),
       ssl: {
         ca: Buffer.from(caCert),
